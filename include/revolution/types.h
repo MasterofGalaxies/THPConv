c77d7cc1752764330c65c97f14ef3edb9373904d
Galaxy Master <76506954+MasterofGalaxies@users.noreply.github.com>
2021-09-18 21:38:07 -0400

    revolution/types.h: improve formatting and make "GCC" default

--- include/revolution/types.h
+++ include/revolution/types.h
@@ -128,18 +128,7 @@ typedef unsigned char       u8;
 typedef unsigned short      u16;
 typedef unsigned long       u32;
 typedef unsigned long long  u64;
-#else       // __MWERKS__
-#ifdef X86  // GCC
-typedef unsigned long long  u64;
-typedef   signed long long  s64;
-typedef unsigned int        u32;
-typedef   signed int        s32;
-typedef unsigned short      u16;
-typedef   signed short      s16;
-typedef unsigned char       u8;
-typedef   signed char       s8;
-#else
-#ifdef __SN__               // for SN
+#elif defined(__SN__)       // for SN
 typedef   signed char       s8;
 typedef   signed short      s16;
 typedef   signed long       s32;
@@ -148,8 +137,7 @@ typedef unsigned char       u8;
 typedef unsigned short      u16;
 typedef unsigned long       u32;
 typedef unsigned long long  u64;
-#else // __SN__
-// win32 style
+#elif defined(_WIN32)       // win32 style
 typedef __int8              s8;
 typedef __int16             s16;
 typedef __int32             s32;
@@ -158,9 +146,16 @@ typedef unsigned __int8     u8;
 typedef unsigned __int16    u16;
 typedef unsigned __int32    u32;
 typedef unsigned __int64    u64;
-#endif      // __SN__
-#endif      // X86
-#endif      // __MWERKS__
+#else
+typedef unsigned long long  u64;
+typedef   signed long long  s64;
+typedef unsigned int        u32;
+typedef   signed int        s32;
+typedef unsigned short      u16;
+typedef   signed short      s16;
+typedef unsigned char       u8;
+typedef   signed char       s8;
+#endif
 
 typedef volatile u8         vu8;
 typedef volatile u16        vu16;
